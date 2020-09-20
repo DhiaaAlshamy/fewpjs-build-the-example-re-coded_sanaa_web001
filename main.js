66eb197a4b05ba46f3341064df198fd33b9e511a
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-20 13:41:42 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
-document.querySelector('.hiddin')
+document.getElementById('modal').classes.add('hiddin');
 
 
 //------------------------------------------------------------------------------
